db90287f8c684929ca34bd4ec638ef6498b8c8d0
jhonastba94 <jhonastba.v2@gmail.com>
2023-02-02 13:21:22 -0500

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# DEMO27ANGULAR
+# Demo19Angular
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.0.4.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'DEMO27-ANGULAR'`, () => {
+  it(`should have as title 'demo19-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('DEMO27-ANGULAR');
+    expect(app.title).toEqual('demo19-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('DEMO27-ANGULAR app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('demo19-angular app is running!');
   });
 });
